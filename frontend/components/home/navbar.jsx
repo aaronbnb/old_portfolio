7a78a90ae1c00c735f10cf1f92307125ce12d7e7
Aaron Farber <aaronfarber9@gmail.com>
2017-08-29 00:11:36 -0700

    changed front-end routing/component hierarchy

--- frontend/components/home/navbar.jsx
+++ frontend/components/home/navbar.jsx
@@ -4,21 +4,11 @@ import {withRouter, Link, browserHistory, hashHistory } from 'react-router';
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
-    this.linkAndToggle = this.linkAndToggle.bind(this);
     this.toggle = this.props.toggle.bind(this);
-
   }
 
   componentDidMount() {
     this.props.fetchBlogs();
-  }
-
-  linkAndToggle(path) {
-    return e => {
-      console.log(path);
-      this.props.toggle(path);
-      hashHistory.push(path);
-    };
   }
 
   render() {
@@ -59,36 +49,3 @@ class NavBar extends React.Component {
 }
 
 export default NavBar;
-
-//
-// <div className='moac-container'>
-//   <nav className="navbar navbar-default">
-//     <div className="container-fluid">
-//       <div className="navbar-header">
-//
-//         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-//           <span className="sr-only">Toggle navigation</span>
-//           <span className="icon-bar"></span>
-//           <span className="icon-bar"></span>
-//           <span className="icon-bar"></span>
-//         </button>
-//         <Link className="navbar-brand" href="#"></Link>
-//
-//       </div>
-//       <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-//         <div className="choice-box">
-//           <div className='expand-effect'>
-//             <ul className="nav navbar-nav">
-//
-//               <li><a href="#">&nbsp;Developer &nbsp;<span className="sr-only">(current)</span></a></li>
-//               <li><a href="#">&nbsp;Teacher &nbsp;</a></li>
-//               <li><a href="#">&nbsp;Resume &nbsp;</a></li>
-//               <li><a href="#">&nbsp;Musings &nbsp;</a></li>
-//
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </nav>
-// </div>
