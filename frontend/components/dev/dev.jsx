5e74d3c84f3b8f5dbe670b626ceea8893cdd61d5
Aaron Farber <aaronfarber9@gmail.com>
2017-08-30 15:17:04 -0700

    extraneous component on skils

--- frontend/components/dev/dev.jsx
+++ frontend/components/dev/dev.jsx
@@ -11,24 +11,73 @@ class Dev extends React.Component {
     return (
       <div className='dev-container'>
         <div className='skills'>
-          <ul className="skill-list">
-            <li className="skill-list-item">HTML</li>
-            <li className="skill-list-item">CSS</li>
-            <li className="skill-list-item">JavaScript</li>
-            <li className="skill-list-item">Ruby On Rails</li>
-            <li className="skill-list-item">React/Redux</li>
-            <li className="skill-list-item">React Native</li>
-            <li className="skill-list-item">SQL</li>
-            <li className="skill-list-item">Web Accessibility</li>
-            <li className="skill-list-item">Webpack</li>
-            <li className="skill-list-item">Bootstrap</li>
-            <li className="skill-list-item">Google Cloud</li>
-            <li className="skill-list-item">D3</li>
-          </ul>
-        </div>
-      </div>
+
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <div id="html5-icon"></div>
+                  HTML5
+                </li>
+                <li className="list-group-item">
+                  <div id="css3-icon"></div>
+                  CSS3
+                </li>
+                <li className="list-group-item">
+                  <div id="js-icon"></div>
+                  JavaScript
+                  <span id="es6">ES6</span>
+                </li>
+                <li className="list-group-item">
+                  <div id="access-icon"></div>
+                  Web Accessibility
+                </li>
+                <li className="list-group-item">
+                  <div id="gcloud-icon"></div>
+                  Google Cloud
+                </li>
+                <li className="list-group-item">
+                  <div id="react-icon"></div>
+                  React / React Native
+                </li>
+                <li className="list-group-item">
+                  <div id="ror-icon"></div>
+                  Ruby on Rails
+                </li>
+                <li className="list-group-item">
+                  <div id="sql-icon"></div>
+                  SQL
+                </li>
+              </ul>
+
+                    </div>
+                  </div>
     );
   }
 }
 
 export default Dev;
+
+//
+// <li className="list-group-item justify-content-between">
+//   Ruby On Rails
+//   <span className="badge badge-default badge-pill">1</span>
+// </li>
+// <li className="list-group-item justify-content-between">
+//   React/React Native
+//   <span className="badge badge-default badge-pill">1</span>
+// </li>
+// <li className="list-group-item justify-content-between">
+//   Bootstrap
+//   <span className="badge badge-default badge-pill">1</span>
+// </li>
+// <li className="list-group-item justify-content-between">
+//   SQL
+//   <span className="badge badge-default badge-pill">1</span>
+// </li>
+// <li className="list-group-item justify-content-between">
+//   Data Visualization
+//   <span className="badge badge-default badge-pill">1</span>
+// </li>
+// <li className="list-group-item justify-content-between">
+//   Web Accessibility
+//   <span className="badge badge-default badge-pill">1</span>
+// </li>
