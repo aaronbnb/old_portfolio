09283120af29245b24d8b3df52e399a35c34fd09
Aaron Farber <aaronfarber9@gmail.com>
2017-08-31 00:26:28 -0700

    redone dev container

--- frontend/components/dev/dev.jsx
+++ frontend/components/dev/dev.jsx
@@ -17,19 +17,21 @@ class Dev extends React.Component {
           <div className="card-block">
             <div className="top-box">
               <div className="project-pic"></div>
+              <div className="project-links">
+                <a href="#" className="card-link">
+                  <i id="link-icon" className="fa fa-link" aria-hidden="true"></i>
+                </a>
+                <a href="#" className="card-link">
+                  <i id="gh-icon" className="fa fa-github" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
               <div className="project-tagline">
                 <h4 className="card-title">Indieclono</h4>
                 <h6 className="card-subtitle mb-2 text-muted">HTML/CSS, JavaScript, React/Redux, Rails, Webpack</h6>
+                <p className="card-text">A full-stack fundraising platform inspired by Indiegogo</p>
               </div>
-          </div>
-            <p className="card-text">A full-stack fundraising platform inspired by Indiegogo</p>
-            <a href="#" className="card-link">
-              <i id="link-icon" className="fa fa-link" aria-hidden="true"></i>
-            </a>
-            <a href="#" className="card-link">
-              <i id="gh-icon" className="fa fa-github" aria-hidden="true"></i>
-            </a>
-          </div>
+            </div>
         </div>
 
         <div className="card">
