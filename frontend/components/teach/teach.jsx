7cdacbc2c8408ff3a5f4f66e5fe4efd950de5478
Aaron Farber <aaronfarber9@gmail.com>
2017-09-04 18:38:41 -0700

    refactoring component sidebar

--- frontend/components/teach/teach.jsx
+++ frontend/components/teach/teach.jsx
@@ -11,45 +11,34 @@ class Teach extends React.Component {
 
     return(
       <div className="teach-container">
+        <div id="teach-text">
+          <div className="test-prep-bio-container">
+
+            <p className="lead">Colleges and grad schools weigh these four hour tests sometimes more
+              for four years of school. Standardized testing is an opportunity to learn and dramatically boost your applications
+            in a short time.</p>
+            <p id="test-prep-bio">Test prep doesn't have to be just another hurdle to admission.
+            For my test prep classes, students learn new ways to interpret arguments and bridge the gap between
+            equations and the world around us. I believe test prep has value far beyond the tests.
+            And no matter what happens, you can never take any someone's knowledge. It is power.</p>
+          <p id="test-prep-bio">I've worked for years as an SAT, ACT, GRE Instructor for Kaplan Test Prep.
+          I'm ranked as an <em>All-Star</em> Teacher for high teacher performance ratings and student
+          satisfaction. Beyond tutoring, I go to bat for my students. I help in any way I can--reviewing personal statements,
+          recommending application strategies, and identifying extracurricular opportunites of the most value.</p>
+            <p id="test-prep-bio">I have some core assumptions about test prep, teaching, and learning that are the root of my work.</p>
+            <ul id="test-prep-assumptions">
+              <li>Students and teachers have a two-way relationship. Students take their lead from their teachers. However much students give,
+                I give it back to them. </li>
+              <li>Students are not good at math but then bad at writing or bad at math but good at verbal.
+              That's not how the brain works. If a student is strong at one subject, I've learned that they
+              need to understand the other subjects in the same way. The skills that make someone strong at math or at writing
+              apply to the other discipline just as well. It just takes direction.</li>
+            <li>Humor is a powerful tool for learning. When a student can laugh about a topic or a concept,
+            it means <em>they get it</em>. Yes, I'm an animated dude and I carry over to the classroom.</li>
+            </ul>
+          </div>
+
         <TeachSidebar />
-
-
-
-        <div className="jumbotron">
-  <h1 className="display-3">Hello, world!</h1>
-  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr className="my-4"/>
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p className="lead">
-    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-  </div>
-
-        <div className="test-prep-bio-container">
-          <p id="test-prep-bio">Colleges and grad schools weigh these four hour tests sometimes more
-            for four years of school. Standardized testing is an opportunity to learn and dramatically boost your applications
-          in a short time.</p>
-          <p id="test-prep-bio">Test prep doesn't have to be just another hurdle to admission.
-          For my test prep classes, students learn new ways to interpret arguments and bridge the gap between
-          equations and the world around us. I believe test prep has value far beyond the tests.
-          And no matter what happens, you can never take any someone's knowledge. It is power.</p>
-        <p id="test-prep-bio">I've worked for years as an SAT, ACT, GRE Instructor for Kaplan Test Prep.
-        I'm ranked as an <em>All-Star</em> Teacher for high teacher performance ratings and student
-        satisfaction. Beyond tutoring, I go to bat for my students. I help in any way I can--reviewing personal statements,
-        recommending application strategies, and identifying extracurricular opportunites of the most value.</p>
-        </div>
-        <div>
-          <p id="test-prep-bio">I have some core assumptions about test prep, teaching, and learning that are the root of my work.</p>
-          <ul id="test-prep-assumptions">
-            <li>Students and teachers have a two-way relationship. Students take their lead from their teachers. However much students give,
-              I give it back to them. </li>
-            <li>Students are not good at math but then bad at writing or bad at math but good at verbal.
-            That's not how the brain works. If a student is strong at one subject, I've learned that they
-            need to understand the other subjects in the same way. The skills that make someone strong at math or at writing
-            apply to the other discipline just as well. It just takes direction.</li>
-          <li>Humor is a powerful tool for learning. When a student can laugh about a topic or a concept,
-          it means <em>they get it</em>. Yes, I'm an animated dude and I carry over to the classroom.</li>
-          </ul>
         </div>
       </div>
     );
